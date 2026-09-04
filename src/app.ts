c1ebb08e65d94dc86a86c56ae3bf8a42d7131993
Fahmida0010 <fahmidaakter0010@gmail.com>
2026-09-04 16:51:03 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Load shedding Server is running!");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
